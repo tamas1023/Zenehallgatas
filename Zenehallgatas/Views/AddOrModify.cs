2e5110fb25b0380e322b076a7013f6b2d9e800af
tamas1023 <bodnar.tamas.1023@gmail.com>
2024-11-06 20:14:00 +0100

    Add project files.

---
+++ Zenehallgatas/Views/AddOrModify.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zenehallgatas.Views
+{
+    public partial class AddOrModify : Form
+    {
+        public AddOrModify()
+        {
+            InitializeComponent();
+        }
+    }
+}
